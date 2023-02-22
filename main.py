ccb53b537f92c8c4bd89283df1975452f55b6ed2
Trash-Child <dicte.mj.98@gmail.com>
2023-02-22 10:19:00 +0100

    Update end of line sequence

--- main.py
+++ main.py
@@ -18,17 +18,7 @@ ev3 = EV3Brick()
 # Initialising motor at port a
 motor_a = Motor(Port.A)
 
-# Initialising motor at port b
-motor_b = Motor(Port.B)
-
-# Initialising motor at port c
-motor_c = Motor(Port.C)
-
-# Initialising motor at port d
-motor_d = Motor(Port.D)
-
-
 #####--FUNCTIONAL CODE--#####
 
 #run motor at 500 degress pr second
-motor_a.run_target(500)
+motor_a.run_target(500,360)
