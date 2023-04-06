421fb32869b93a7ed33015273c0b347c87d53dcf
Anton <boblo.ludvigsen@gmail.com>
2023-04-06 18:42:39 +0200

    Binary image with 20% brightness shown as white

--- Camera/Cameramain.py
+++ Camera/Cameramain.py
@@ -1,26 +1,32 @@
 import cv2
+import numpy as np
 
-#Code made following guide https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
+# Code made following guide https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
 vid = cv2.VideoCapture(0)
 
 def camera():
     while(True):
+        # Capture vid frame by frame
+        ret, frame = vid.read()
 
-        #Capture vid frame by frame
-        ret, frame=vid.read()
+        # Convert frame to grayscale
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-       #display of frame
-        cv2.imshow('frame', frame)
+        # Calculate threshold value
+        max_intensity = np.max(gray)
+        threshold = int(0.8 * max_intensity)
 
-       #s button to stop display
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+        # Threshold the grayscale image
+        _, binary = cv2.threshold(gray, threshold, 255, cv2.THRESH_BINARY)
+
+        # Display the binary image
+        cv2.imshow('Binary', binary)
+
+        # 'q' button to stop display
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-    
 
     vid.release()
     cv2.destroyAllWindows()
 
-
 camera()
-
-
