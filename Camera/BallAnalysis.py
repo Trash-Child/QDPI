675ffde481fe7ce5cbb715d9ca4d8fd140809a0c
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2023-04-12 13:55:17 +0200

    minor changes, cosmetic

--- Camera/BallAnalysis.py
+++ Camera/BallAnalysis.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-# Code made following guide https://www.geeksforgeeks.org/python-opencv-capture-video-from-camera/
+
 vid = cv2.VideoCapture(0)
 
 def camera():
@@ -23,10 +23,10 @@ def camera():
             circles = np.round(circles[0, :]).astype("int")
             for (x, y, r) in circles:
                 # Filter out small circles
-                if r > 10:  # Set your minimum radius threshold here
+                if r > 10:  # Minimum threshhold
                     # Extract region of interest (ROI) within the circle
                     roi = frame[y-r:y+r, x-r:x+r]
-                    print("ROI shape:", roi.shape)  # Debugging statement
+                    print("ROI shape:", roi.shape)  # Debugging purposes
 
                     # Check if ROI is not None and has non-empty shape
                     if roi is not None and roi.shape[0] > 0 and roi.shape[1] > 0:
@@ -34,7 +34,7 @@ def camera():
                         roi_gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
 
                         # Apply binary thresholding to extract white regions
-                        _, thresh = cv2.threshold(roi_gray, 220, 255, cv2.THRESH_BINARY)
+                        _, thresh = cv2.threshold(roi_gray, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, 255, cv2.THRESH_BINARY)
 
                         # Count the number of white pixels in the thresholded image
                         white_pixels = cv2.countNonZero(thresh)
@@ -47,7 +47,7 @@ def camera():
 
                         # Set a threshold for roundness
                         roundness_threshold = 0.8
-
+                            
                         # Set a threshold for circularity based on area comparison
                         area_threshold = 0.8
 
@@ -60,8 +60,8 @@ def camera():
         # Display the frame with detected circles
         cv2.imshow('frame', frame)
 
-        # Press 's' key to stop display
-        if cv2.waitKey(1) & 0xFF == ord('s'):
+        # Press 'q' key to stop display
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
     vid.release()
