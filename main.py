5481fd6705f86434a2d2cd6648edd5fe44f5a6a6
Maria Stentoft <s195685@student.dtu.dk>
2023-06-05 09:44:12 +0200

    yellow pls

--- main.py
+++ main.py
@@ -16,26 +16,67 @@ from threading import Thread
 # Initialising the brick
 ev3 = EV3Brick()
 
-# Initialising motor at port a
-motor_a = Motor(Port.A)
-motor_b = Motor(Port.B)
+#This program requires LEGO EV3 MicroPython v2.0 or higher.
+#Click "Open user guide" on the EV3 extension tab for more information.
+#Initialising motor at ports a+b
+#r is right, l is left
+motor_r = Motor(Port.A)
+motor_l = Motor(Port.B)
+motor_pickupWheels = Motor(Port.C)
+motor_v = Motor(Port.D)
 
-#####--FUNCTIONAL CODE--#####
+#Drivebase has LEFT motor first!
+qdpi = DriveBase(motor_l,motor_r,wheel_diameter=32.5,axle_track=204.5)
+#larvefod diameter 32.5
 
-#move straight for time duration. 
+##### testing thingamajig######
 
-#duration specified in timesp, speed_sp necessary 
-motor_a.speed_sp = 1000
-motor_a.time_sp = 500
-# motor_a.run_target(500,360)
+#variables
+
+#angle value gets updated by some other section so we know how many degrees it has to turn
+angle = 45
+#timerot value gets updated with how much time it turns to that angle(?)
+timerot = 1000
+#basespeed is the basespeed dumbass 
+basespeed = 150
+#drive will define if it drives
+drive = 1
+
+def qdpidrive(drive):
+
+    while drive==1: 
+        qdpi.drive(150,0)
+        break
 
 
-buttons = Button()
-def checking_interrupt():
-    global buttons
-    while buttons.space:
-        motor_a.run_timed()
+def qdpirotate(angle):
+    
+    qdpi.drive(0,angle)
+    
 
-motor_b.speed_sp = 1000
-motor_b.time_sp = 500
-motor_b.run_timed()
+def qdpiopen():
+
+    motor_v.run(100)
+    wait(1000)
+    motor_v.stop()
+
+
+   
+
+##run rotate program until correctly placed
+
+motor_pickupWheels.run(500)
+
+qdpidrive(drive=1)
+wait(10000)
+qdpidrive(drive=0)
+qdpirotate(angle=45)
+wait(1000)
+qdpirotate(angle=0)
+wait(1000)
+qdpiopen()
+qdpipause()
+
+
+brick.sound.beeps(2)
+
