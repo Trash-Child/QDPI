3e6c64d88bb1cbb0f5fd39eec4d81e8c2133231c
Magnusbruus <magnusbruus@gmail.com>
2023-06-06 13:21:21 +0200

    HoughLinesP implemented. line coordinates span

--- Camera/BallAnalysis.py
+++ Camera/BallAnalysis.py
@@ -45,21 +45,12 @@ def detect_walls(frame):
     # Apply Canny edge detection
     edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-    # Apply Hough Line Transform
-    lines = cv2.HoughLines(edges, 1, np.pi / 180, 200)
+    # Apply Probabilistic Hough Line Transform
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
     walls = []
     if lines is not None:
-        for rho, theta in lines[:,0]:
-            a = np.cos(theta) # x part of unit vector
-            b = np.sin(theta) # y part of unit vector
-            x0 = a * rho # x coordinate for line's intersection with y-axis
-            y0 = b * rho # y coordinate for line's intersection with x-axis
-
-            # Find two far apart points (10000 pixels apart)
-            x1 = int(x0 + 10000 * -b)
-            y1 = int(y0 + 10000 * a)
-            x2 = int(x0 - 10000 * -b)
-            y2 = int(y0 - 10000 * a)
+        for line in lines:
+            x1, y1, x2, y2 = line[0]
 
             # Add the wall's endpoints to the array
             walls.append(((x1, y1), (x2, y2)))
