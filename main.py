86bc7a0d88e28b0f42a1b39b9e4099f84c7bfcaf
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2023-06-07 13:11:24 +0200

    Created a server-client relationship between EV3 and PC. They need to be on the same network, and you need to make sure the IP address is correct

--- main.py
+++ main.py
@@ -1,87 +1,68 @@
 #!/usr/bin/env pybricks-micropython
-from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
 from threading import Thread
+from pybricks.hubs import EV3Brick as brick 
+from pybricks.ev3devices import Motor, TouchSensor, ColorSensor, InfraredSensor, UltrasonicSensor, GyroSensor
+from pybricks.parameters import Port, Stop, Direction, Button, Color 
+from pybricks.tools import wait, StopWatch, DataLog 
+from pybricks.robotics import DriveBase 
+from pybricks.media.ev3dev import SoundFile, ImageFile 
+import math
+import socket
 
-
-# This program requires LEGO EV3 MicroPython v2.0 or higher.
-# Click "Open user guide" on the EV3 extension tab for more information.
-
-
-# Initialising the brick
-ev3 = EV3Brick()
-
-#This program requires LEGO EV3 MicroPython v2.0 or higher.
-#Click "Open user guide" on the EV3 extension tab for more information.
-#Initialising motor at ports a+b
-#r is right, l is left
-
-motor_r = Motor(Port.A)
-motor_l = Motor(Port.B)
-motor_pickupWheels = Motor(Port.C)
+ev3 = brick()
 motor_v = Motor(Port.D)
-gyro = GyroSensor(Port.S1)
+
+def start_server(port):
+    print("Starting server...")
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server_socket.bind(('', port))
+    server_socket.listen(1)
+    print("Server started.")
+    return server_socket
+
+def handle_client(server_socket):
+    print("Waiting for a client...")
+    client_socket, client_address = server_socket.accept()
+    print("Client connected:", client_address)
+    data = client_socket.recv(1024)
+    reply = data.decode()
+    try:
+        message = b'Thank you for connecting'
+        while message:
+            bytes_sent = client_socket.send(message)
+            message = message[bytes_sent:]
+    except Exception as e:
+        print('Error: {}'.format(e))
+    client_socket.close()
+    return reply
+
+def handle_data(data):
+    if data == '1':
+        motor_v.run(100)
+        wait(1000)
+        motor_v.stop()
 
 
-#Drivebase has LEFT motor first!
-qdpi = DriveBase(motor_l,motor_r,wheel_diameter=32.5,axle_track=204.5)
-#larvefod diameter 32.5
+def run_server():
+    port = 1234
+    server_socket = start_server(port)
 
-##### testing thingamajig######
+    while True:  # Continuously accept new connections.
+        data = handle_client(server_socket)
+        handle_data(data)
+        print('Server received:', data)
 
-#variables
+print("Starting server thread...")
+server_thread = Thread(target=run_server)
+server_thread.start()
 
-#angle value gets updated by some other section so we know how many degrees it has to turn
-angle = 45
-#timerot value gets updated with how much time it turns to that angle(?)
-timerot = 1000
-#basespeed is the basespeed dumbass 
-basespeed = 150
-#drive will define if it drives
-drive = 1
-
-def qdpidrive(drive):
-
-    while drive==1: 
-       #gyro correction along with driving. 
-        correction = (0 - gyro.angle())*1
-        qdpi.drive(150,correction)
-        break
-
-
-def qdpirotate(angle):
-    
-    qdpi.drive(0,angle)
-    
-
-def qdpiopen():
-
-    motor_v.run(100)
-    wait(1000)
-    motor_v.stop()
-   
-
-##run rotate program until correctly placed
-
-motor_pickupWheels.run(500)
-#gyro reset angle!! this has to run first
-gyro.reset_angle(0)
-
-qdpidrive(drive=1)
-wait(10000)
-qdpidrive(drive=0)
-qdpirotate(angle=45)
-wait(1000)
-qdpirotate(angle=0)
-wait(1000)
-qdpiopen()
-qdpipause()
-
-
-brick.sound.beeps(2)
-
+# Add a delay to keep the main program running for a while
+# and prevent it from ending immediately.
+import time
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    print("Shutting down...")
+print("End of main program.")
