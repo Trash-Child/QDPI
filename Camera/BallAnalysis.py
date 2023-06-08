3e614effa44cb71303da7b0fc2d29c0f78c79a30
Anton <boblo.ludvigsen@gmail.com>
2023-06-08 12:44:30 +0200

    Changed a lot of the BallAnalysis logic. Main func is now the driving force. camera() changed to analyseFrame()

--- Camera/BallAnalysis.py
+++ Camera/BallAnalysis.py
@@ -3,8 +3,6 @@ import numpy as np
 import time
 import threading
 
-vid = cv2.VideoCapture(0)
-
 def update_continuous_balls(circle, continuous_balls, position_error_margin, size_error_margin, counter_threshold=10, no_match_threshold=10):
     x, y, r = circle
     updated_continuous_balls = []
@@ -49,65 +47,58 @@ def locateOrangeBall(frame):
         perimeter = cv2.arcLength(cnt, True)
         if perimeter == 0:
             continue
+
         circularity = 4*np.pi*(area / (perimeter**2))
-        if area > best_area and circularity > best_circularity:
-            M = cv2.moments(cnt)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                best_location = cX, cY
-                best_area = area
-                best_circularity = circularity
+
+        if area =< best_area and circularity =< best_circularity:
+            continue
+        
+        M = cv2.moments(cnt)
+        if M["m00"] == 0:
+            continue
+        
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        best_location = cX, cY
+        best_area = area
+        best_circularity = circularity
 
     return best_location
 
 
 
 def locateColoredBall(frame, lower_color, upper_color):
-    # Initialize variables to store the best location, area, and circularity
     best_location = []
     best_area = 0
     best_circularity = 0
-
-    # Convert the image from BGR color space to HSV color space
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    
-    # Create a binary mask where the pixels within the specified color range are set to 1 and others are set to 0
     mask = cv2.inRange(hsv, lower_color, upper_color)
     
-    # Find contours in the mask image
     contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    # Iterate over the found contours
     for cnt in contours:
-        # Calculate the area of the current contour
         area = cv2.contourArea(cnt)
-        
-        # Calculate the perimeter (also known as the arc length) of the contour
         perimeter = cv2.arcLength(cnt, True)
         
-        # Avoid division by zero
         if perimeter == 0:
             continue
         
-        # Calculate the circularity of the contour, which is defined as the ratio of the area to the square of the perimeter
         circularity = 4*np.pi*(area / (perimeter**2))
         
-        # If the current contour has a larger area and better circularity than the best found so far, update the best contour
-        if area > best_area and circularity > best_circularity:
-            # Compute the moments of the contour which can be used to compute the centroid or “center of mass” of the contour
-            M = cv2.moments(cnt)
-            
-            # Avoid division by zero
-            if M["m00"] != 0:
-                # Calculate the x and y coordinates of the center of the contour
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                
-                # Update the best location, area, and circularity
-                best_location = cX, cY
-                best_area = area
-                best_circularity = circularity
+        if area <= best_area and circularity <= best_circularity:
+            continue
+
+        M = cv2.moments(cnt)
+        
+        if M["m00"] == 0:
+            continue
+
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        
+        best_location = cX, cY
+        best_area = area
+        best_circularity = circularity
 
     # Return the center of the best contour
     return best_location
@@ -123,8 +114,8 @@ def locateGreenBall(frame):
 
 
 def locateBlueBall(frame):
-    lower_blue = np.array([80, 20, 50])  # Lower the saturation to target lighter blue colors
-    upper_blue = np.array([130, 255, 255])  # You can adjust this value depending on the exact shade you're trying to detect
+    lower_blue = np.array([80, 20, 50])
+    upper_blue = np.array([130, 255, 255])
 
     return locateColoredBall(frame, lower_blue, upper_blue)
 
@@ -132,34 +123,31 @@ def locateBlueBall(frame):
 
 closest_ball = [None]
 
-def camera():
+def analyseFrame(frame):
     global closest_ball
     continuous_balls = []
     position_error_margin = 25
     size_error_margin = 3
 
-    for i in range(1):
-        ret, frame = vid.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        _, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)  # Filter out low light pixels
+    gray = cv2.threshold(frame, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 220, 255, cv2.THRESH_BINARY)  # Filter out low light pixels
 
-        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         
-        for cnt in contours:
+    for cnt in contours:
+        # Find the center of the white part
+        M = cv2.moments(cnt)
+        if M["m00"] != 0:
+            cX = int(M["m10"] / M["m00"])
+            cY = int(M["m01"] / M["m00"])
+            r = np.sqrt(cv2.contourArea(cnt) / np.pi)
+            
+            continuous_balls = update_continuous_balls((cX, cY, r), continuous_balls, position_error_margin, size_error_margin)
+            cv2.circle(frame, (cX, cY), int(r), (0, 255, 0), 2)
+            cv2.circle(frame, (cX, cY), 2, (0, 0, 255), 3)
 
-            # Find the center of the white part
-            M = cv2.moments(cnt)
-            if M["m00"] != 0:
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                r = np.sqrt(cv2.contourArea(cnt) / np.pi)
-                
-                continuous_balls = update_continuous_balls((cX, cY, r), continuous_balls, position_error_margin, size_error_margin)
-                cv2.circle(frame, (cX, cY), int(r), (0, 255, 0), 2)
-                cv2.circle(frame, (cX, cY), 2, (0, 0, 255), 3)
-                print("camera: closest ball = ", closest_ball)
-                if closest_ball[0] and cX == closest_ball[0][0] and cY == closest_ball[0][1]:
-                    cv2.circle(frame, (cX, cY), int(r) + 10, (255, 0, 0), 4)  # Draw an extra circle around the closest ball
+            if closest_ball[0] and cX == closest_ball[0][0] and cY == closest_ball[0][1]:
+                cv2.circle(frame, (cX, cY), int(r) + 10, (255, 0, 0), 4)  # Draw an extra circle around the closest ball
 
         # Find green and blue balls, and draw a circle around them
         green_ball_location = locateGreenBall(frame)
@@ -169,55 +157,61 @@ def camera():
         blue_ball_location = locateBlueBall(frame)
         if blue_ball_location:
             cv2.circle(frame, blue_ball_location, 10, (0, 0, 255), 2)
-        cv2.imshow('frame', frame)
-
+        
         orange_ball_location = locateOrangeBall(frame)
 
     return continuous_balls, orange_ball_location, green_ball_location, blue_ball_location
 
 
-def locate_nearest_ball():
+def locate_nearest_ball(continuous_balls, orange_ball_location, green_dot, blue_dot):
     global closest_ball
-    while True:
-        continuous_balls, orange_ball_location, green_dot, blue_dot = camera()
-        # define robot as the middle of the green dot and blue dot
-        if green_dot and blue_dot:
-            robot = ((green_dot[0] + blue_dot[0]) / 2, (green_dot[1] + blue_dot[1]) / 2)
-        else:
-            print("Green dot or blue dot not found!")
-            return None
 
-        # initial closest distance set to a large number
-        closest_distance = float('inf')
-        closest_ball = None
+    if not green_dot or not blue_dot:
+        print("Robot not found")
+        return None
 
-        # check each ball in continuous_balls to find the closest
-        for ball in continuous_balls:
-            distance = ((robot[0] - ball[0])**2 + (robot[1] - ball[1])**2)**0.5
-            if distance < closest_distance:
-                closest_distance = distance
-                closest_ball[0] = ball
+    # define robot as the middle of the green dot and blue dot
+    robot = ((green_dot[0] + blue_dot[0]) / 2, (green_dot[1] + blue_dot[1]) / 2)
+    
+    closest_distance = float('inf')
+    closest_ball = None
+    
+    for ball in continuous_balls:
+        distance = ((robot[0] - ball[0])**2 + (robot[1] - ball[1])**2)**0.5
+        if distance < closest_distance:
+            closest_distance = distance
+            closest_ball = ball
+    # check orange ball if exists
+    if not orange_ball_location:
+        return closest_ball
 
-        # check orange ball if exists
-        if orange_ball_location:
-            distance = ((robot[0] - orange_ball_location[0])**2 + (robot[1] - orange_ball_location[1])**2)**0.5
-            if distance < closest_distance:
-                closest_distance = distance
-                closest_ball[0] = orange_ball_location
-        
-        
-        key = cv2.waitKey(1) & 0xFF
-        if key == ord('q'):
-            break
+    distance = ((robot[0] - orange_ball_location[0])**2 + (robot[1] - orange_ball_location[1])**2)**0.5
+    if distance < closest_distance:
+        closest_distance = distance
+        closest_ball = orange_ball_location
 
-    vid.release()
-    cv2.destroyAllWindows()
-
-    # return closest ball
     return closest_ball
 
 camera_thread = threading.Thread(target=camera)
 
 camera_thread.start()
-locate_nearest_ball()
 
+def main():
+    vid = cv2.VideoCapture(0)
+
+    while True:
+        ret, frame = vid.read()
+
+        analysed_frame = analyseFrame(frame)
+        target = locate_nearest_ball(analysed_frame)
+        if target is not None:
+            cv2.circle(frame, target, 10, (255, 255, 0), 2)
+
+        cv2.imshow('frame', frame)
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord('q'):
+            break
+    
+    vid.release()
+    cv2.destroyAllWindows()
