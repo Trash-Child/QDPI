e5334018ab5eb463152b6f39630e741096a6abb5
Bertram Kjaer <s215775@student.dtu.dk>
2023-06-19 16:48:56 +0200

    added commetns to code

--- client.py
+++ client.py
@@ -1,56 +1,64 @@
-import socket
-import cv2
-from movementLogic import calculateCommand
+import socket  # Importing socket library for network communication
+import cv2  # Importing OpenCV library for video processing
+from movementLogic import calculateCommand  # Importing calculateCommand function from custom module movementLogic
 import time
 import errno
 import numpy as np
 
 # Function to start the client and connect to the server
 def start_client(SERVER_IP, SERVER_PORT):
+    # Creating a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Connecting to the server at the specified IP address and port
     client_socket.connect((SERVER_IP, SERVER_PORT))
-    print(f"Created socket: {client_socket}")  # Debug print
-    return client_socket
+    print(f"Created socket: {client_socket}")  # Printing the socket object for debugging
+    return client_socket  # Returning the socket object
 
-# Function to send data to the server and receive a reply
 # Function to send data to the server and receive a reply
 def send_data(client_socket, data):
-    data = str(data)  # Ensure that data is a string
-    data = data.encode()  # Convert data to bytes
+    data = str(data)  # Ensuring that data is a string
+    data = data.encode()  # Encoding the string data to bytes
     total_sent = 0
+    # Keep sending data until all is sent
     while total_sent < len(data):
+        # Sending a chunk of data
         sent = client_socket.send(data[total_sent:])
-        if sent == 0:  # Socket connection broken
+        # If nothing is sent, socket connection is probably broken
+        if sent == 0:
             raise RuntimeError("Socket connection broken")
         total_sent += sent
+    # Receiving the reply from the server
     reply = client_socket.recv(1024).decode()
-    return reply
+    return reply  # Returning the reply as a string
 
-
-
-# Function to start capturing video
+# Function to start capturing video from the default camera (camera index 0)
 def start_capture():
     vid = cv2.VideoCapture(0)
     return vid
 
-# Function to stop capturing video
+# Function to stop capturing video and release the resources
 def stop_capture(vid):
     vid.release()
     cv2.destroyAllWindows()
 
 def main():
     manual = False
+    # Allowing the user to select manual mode
     if input("Press 1 for manual, or anything else to continue: ") == '1':
         manual = True
     else:
         vid = start_capture()
 
-    SERVER_IP = '172.20.10.4'  # EV3's IP address. Anton: 192.168.43.184 Bertram: 172.20.10.4
-    SERVER_PORT = 1234  # The same port used by the EV3's server.
+    # Defining the server IP and port
+    SERVER_IP = '172.20.10.4'
+    SERVER_PORT = 1234
+    # Starting the client
     client_socket = start_client(SERVER_IP, SERVER_PORT)
     
+    # Main loop
     while True:
         try:
+            # If not in manual mode, capture a frame and calculate a command
             if not manual:
                 ret, frame = vid.read()
                 ret2, debugFrame = vid.read()
@@ -59,13 +67,16 @@ def main():
                 # Convert numpy float to a regular Python float if necessary
                 if isinstance(cmd, np.floating):
                     cmd = float(cmd)
+            # In manual mode, allow user to enter commands
             else:
                 cmd = input("Enter command (1 for drive, >5 for turn, 404 for nothing): ")
             
-            reply = send_data(client_socket, str(cmd))  # Convert cmd to string before sending
+            # Send the command to the server and receive the reply
+            reply = send_data(client_socket, str(cmd))
             print('Sent:', cmd)
             print('Received:', reply)
             
+            # Wait for the server to execute the command
             if reply != 'Command executed':
                 print('Waiting for command execution...')
                 while True:
@@ -73,16 +84,20 @@ def main():
                     if reply == 'Command executed':
                         break
 
+        # Handle exceptions, e.g., in case of a broken connection
         except Exception as e:
+            # Check if the exception is due to a socket error
             if hasattr(e, 'winerror') or hasattr(e, 'errno'):
+                # If connection is closed by the server, break the loop
                 if e.winerror == 10053 or e.errno == errno.WSAECONNRESET:
                     print('Connection closed')
                     break
             print(f"Exception occurred:", e)
-            continue
+            continue  # Continue with the next iteration if an exception occurred
     
+    # Clean up at the end
     stop_capture(vid)
     client_socket.close()
 
 if __name__ == '__main__':
-    main()
+    main()  # Start the main function if the script is run as a standalone program
--- movementLogic.py
+++ movementLogic.py
@@ -1,23 +1,23 @@
+# Import necessary libraries and modules
 from Camera.BallAnalysis import analyseFrame, locate_nearest_ball, findRobot
 from Camera.ObstacleAnalysis import detectX
 import cv2
 import math
 import numpy as np
 
-# This function extracts important information from a frame captured by a camera.
-# It analyzes the frame to locate white balls, orange balls, green balls, and blue balls.
-# It then finds the nearest white ball and the nearest robot ball.
-# If any of the required information is missing, it returns None.
+# Function to extract important information from a frame captured by a camera.
 def getImportantInfo(frame, debugFrame):
     try:
+        # Analyze the frame to locate white balls, orange balls, green balls, and blue balls.
+        # Find the nearest white ball and the nearest robot ball.
         white_balls, orange = analyseFrame(frame, debugFrame)
         robot, heading = findRobot(frame, debugFrame)
         target = locate_nearest_ball(white_balls, orange, robot)
     except Exception as e:
         print(f"Error occurred while analysing frame: {e}")
-        return None, None, None
+        return None, None, None  # If there's an error, return None values
 
-    # Extract the coordinates of the target ball, robot ball, and green ball.
+    # Process the target coordinates
     if target is not None:
         try:
             target = target[0][:2]
@@ -29,6 +29,7 @@ def getImportantInfo(frame, debugFrame):
         print("No target found.")
         target = None
 
+    # Process the robot coordinates
     if robot is not None:
         try:
             robot = int(robot[0]), int(robot[1])
@@ -36,34 +37,32 @@ def getImportantInfo(frame, debugFrame):
             print(f"Error occurred while processing robot: {e}")
             robot = None
 
+    # Return the coordinates of target, robot, and heading
     return target, robot, heading
 
-
-# This function calculates the angle between three points.
+# Function to calculate the angle between three points
 def get_heading_to_ball(ball, robot_pos, robot_heading):
+    # If either the ball or robot's position is None, return None
     if ball is None or robot_pos is None:
         return None
 
+    # Compute the direction vector from the robot to the ball
     direction_vector = np.array([ball[0] - robot_pos[0], ball[1] - robot_pos[1]])
+    # Compute the desired heading in degrees and make sure it is within [0, 360)
     desired_heading = (np.arctan2(direction_vector[1], direction_vector[0]) * 180 / np.pi +180) % 360
+    # Compute the relative angle to the current heading of the robot, and make sure it is within [-180, 180)
     relative_angle = (desired_heading - robot_heading + 180) % 360 - 180
 
     return relative_angle-85
 
-# This function calculates the command based on the given frame.
-# It calls the getImportantInfo function to extract the necessary information.
-# If the robot ball is missing, it returns 404.
-# Otherwise, it calculates the angle between the green ball, robot ball, and target ball.
-# If the angle is greater than 5 degrees, it returns the angle.
-# Otherwise, it returns 1.
-
-waypoints = []  # Global variable to hold the waypoints
-turn_completed = False
-threshold_distance = 10  # Distance at which we consider the waypoint has been reached
+waypoints = []  # Global list to hold the waypoints
+turn_completed = False  # Global flag to indicate if a turn has been completed
+threshold_distance = 10  # Threshold distance to consider a waypoint as reached
 
 def calculateCommand(frame, debugFrame):
-    global waypoints  # Declare waypoints as a global variable
+    global waypoints  # Declare waypoints as global
 
+    # Extract important information from the frame
     try:
         target, robot, robot_heading = getImportantInfo(frame, debugFrame)
         print(robot, robot_heading)
@@ -71,22 +70,24 @@ def calculateCommand(frame, debugFrame):
         print(f"Error when calling getImportantInfo: {e}")
         return None
 
-    # Check if robot or target or robot_heading is None, if so, skip to the next iteration
+    # Check if robot or target or robot_heading is None, if so, return None
     if robot is None or target is None or robot_heading is None:
         print("Skipping iteration due to missing data.")
         return None
 
+    # Get the obstacle information from the frame
     try:
         obstacle_info = getObstacleInfo(frame, debugFrame)
     except Exception as e:
         print(f"Error when calling getObstacleInfo: {e}")
         return None
 
-    # Check if obstacle_info is None, if so, skip to the next iteration
+    # If obstacle info is None, return None
     if obstacle_info is None:
         print("Skipping iteration due to missing obstacle information.")
         return None
 
+    # If there are no waypoints, calculate them
     if not waypoints:
         try:
             waypoints = avoidObstacles(frame, debugFrame, robot, target, obstacle_info, robot_heading)
@@ -94,32 +95,34 @@ def calculateCommand(frame, debugFrame):
             print(f"Error when calling avoidObstacles: {e}")
             return None
 
+    # Get the first waypoint and its heading
     waypoint, waypoint_heading = waypoints[0]
+
+    # Compute the heading to the waypoint
     try:
         angle = get_heading_to_ball(waypoint, robot, robot_heading)
     except Exception as e:
         print(f"Error when calculating turning angle: {e}")
         return None
 
-    if math.hypot(waypoint[0] - robot[0], waypoint[1] - robot[1]) < 10:  # If close enough to waypoint
-        if abs(angle) <= 1:  # And if within the threshold angle
-            waypoints.pop(0)  # Remove this waypoint from the list
+    # If the robot is close enough to the waypoint
+    if math.hypot(waypoint[0] - robot[0], waypoint[1] - robot[1]) < 10:
+        # And if within the threshold angle
+        if abs(angle) <= 1:
+            # Remove this waypoint from the list
+            waypoints.pop(0)
 
+    # If the absolute angle is above 5 degrees, turn towards waypoint/target
     try:
-        if abs(angle) > 5:  # Turn if the absolute angle is above 5 degrees
-            return angle  # Turn towards waypoint/target
-        else:
-            return 1  # Go straight
+        if abs(angle) > 5:
+            return angle
+        else:  # Otherwise, go straight
+            return 1
     except Exception as e:
         print(f"Error occurred during command decision: {e}")
         return None
 
-
-
-
-# This function extracs the 2D vectors of the obstacle lines and stores them for further logic
-# It calls detectX to get the line vectors
-# It returns the line vector array
+# Function to extract obstacle information from the frame
 def getObstacleInfo(frame, debugFrame):
     # Get line vectors of detected obstacles
     lineVectors = detectX(frame, debugFrame)
@@ -131,83 +134,61 @@ def getObstacleInfo(frame, debugFrame):
 
 # Function to check if path to ball is intersected by the obstacle
 def lineIntersection(line1, line2):
-    # line1 and line2 are 2D vectors with the format ((x1, y1), (x2, y2))
-    
-    print(f"line1: {line1}, line2: {line2}")  # Debug
+    # Line1 and line2 are 2D vectors with the format ((x1, y1), (x2, y2))
 
-    # Calculate the difference between the x-coordinates and the y-coordinates
-    # for each line.
+    # Calculate the difference in x and y coordinates for each line
     xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
     ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
 
-    # The function 'det' calculates the determinant of a 2x2 matrix formed by
-    # the two points on the line. This is a measure of the "signed" area parallelogram
-    # spanned by these points. In this context, it's used to help find the
-    # intersection point of the two lines.
+    # Calculate the determinant of xdiff and ydiff to check if lines are parallel
     def det(a, b):
         return a[0] * b[1] - a[1] * b[0]
 
-    # Calculate the determinant of xdiff and ydiff. This checks if the two lines are parallel.
-    # If they are, the determinant will be zero and we return False as there is no intersection point.
     div = det(xdiff, ydiff)
     if div == 0:
        return False
 
-    # Compute the determinants of the two lines. The determinant is a special number that can be calculated
-    # from a matrix. In this case, the matrix is a 2x2 matrix formed by the coordinates of the line's points.
+    # Calculate x and y coordinates of the intersection point
     d = (det(*line1), det(*line2))
-
-    # Calculate the x and y coordinates of the intersection point.
     x = det(d, xdiff) / div
     y = det(d, ydiff) / div
 
-    # Return the intersection point.
+    # Return the intersection point
     return x, y
 
-
 def avoidObstacles(frame, debugFrame, robot, target, line_vectors, heading):
-    # Path is a straight line from robot to target
+    # The path from the robot to the target is initially a straight line
     path = (robot, target)
 
-    waypoints = [(robot, heading)]  # Start with the robot's initial position and its current heading
+    # Start with the robot's initial position and its current heading
+    waypoints = [(robot, heading)]
 
-    # Check if path intersects with any obstacle
+    # Check if the path intersects with any of the obstacles
     for obstacleLine in line_vectors:
-        print(f"Checking obstacle line: {obstacleLine}")  # Debug
-
         intersection_point = lineIntersection(path, obstacleLine)
-        print(f"Intersection point: {intersection_point}")  # Debug
 
+        # If there is an intersection point, we need to adjust our path
         if intersection_point is not None and intersection_point is not False:
-            # Check if intersection_point is a tuple or a list with at least 2 elements.
-            if isinstance(intersection_point, (tuple, list)) and len(intersection_point) >= 2:
-                # Determine the direction of the obstacle relative to the robot
-                if intersection_point[0] < robot[0]:  # Obstacle is to the left
-                    # Create a waypoint to the right of the obstacle
-                    waypoint = (intersection_point[0] + 20, intersection_point[1] + 50)
-                else:  # Obstacle is to the right
-                    # Create a waypoint to the left of the obstacle
-                    waypoint = (intersection_point[0] - 20, intersection_point[1] - 50)
+            # Determine the direction of the obstacle relative to the robot
+            if intersection_point[0] < robot[0]:  # Obstacle is to the left
+                # Create a waypoint to the right of the obstacle
+                waypoint = (intersection_point[0] + 20, intersection_point[1] + 50)
+            else:  # Obstacle is to the right
+                # Create a waypoint to the left of the obstacle
+                waypoint = (intersection_point[0] - 20, intersection_point[1] - 50)
 
-                # Calculate heading to waypoint
-                waypoint_heading = get_heading_to_ball(waypoint, robot, heading)
+            # Calculate heading to waypoint
+            waypoint_heading = get_heading_to_ball(waypoint, robot, heading)
 
-                waypoints.append((waypoint, waypoint_heading))  # Add the waypoint to the list along with its associated heading
-                print(f"New waypoint added: {waypoint}")  # Debug
+            # Add the waypoint to the list along with its associated heading
+            waypoints.append((waypoint, waypoint_heading))
 
-                # Update the robot's path to go from the waypoint to the target
-                path = (waypoint, target)
-                heading = waypoint_heading  # Update heading
-            else:
-                print(f"Unexpected intersection_point: {intersection_point}")
+            # Update the robot's path to go from the waypoint to the target
+            path = (waypoint, target)
+            heading = waypoint_heading  # Update heading
 
     # Add the target to the list of waypoints
-    # Calculate heading to target
     target_heading = get_heading_to_ball(target, robot, heading)
     waypoints.append((target, target_heading))
 
-    print(f"Final waypoints: {waypoints}")  # Debug
     return waypoints
-
-
-
