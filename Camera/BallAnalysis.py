5c2e0df2171f4d9ce4f6b683f17ee28f73131c79
Anton Ludvigsen <boblo.ludvigsen@gmail.com>
2023-06-20 15:18:32 +0200

    Basic check if ball is 'out of bounds'. Probably needs calibration

--- Camera/BallAnalysis.py
+++ Camera/BallAnalysis.py
@@ -235,6 +235,13 @@ def analyseFrame(frame, debugFrame):
             r = np.sqrt(cv2.contourArea(cnt) / np.pi)
             if r < 3:
                 continue
+            
+            if most_frequent_nw:
+                if cX > most_frequent_nw - 10 or cY < most_frequent_nw + 10:
+                    continue
+                if cX < most_frequent_se + 10 or cY > most_frequent_se - 10:
+                    continue
+
             continuous_balls = update_continuous_balls((cX, cY, r), continuous_balls, position_error_margin, size_error_margin)
             cv2.circle(debugFrame, (cX, cY), int(r), (0, 255, 0), 2)
             cv2.circle(debugFrame, (cX, cY), 2, (0, 0, 255), 3)
@@ -253,6 +260,7 @@ def analyseFrame(frame, debugFrame):
     if most_frequent_ne and most_frequent_se:
         mid_e = ((most_frequent_ne[0] + most_frequent_se[0]) // 2, (most_frequent_ne[1] + most_frequent_se[1]) // 2)
         continuous_midpoints["e"], mid_e = update_continuous_midpoints(mid_e, continuous_midpoints["e"], position_error_margin)
+        
     return mid_w, mid_e, continuous_balls, orange_ball_location
 
 
